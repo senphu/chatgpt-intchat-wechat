daf7aedea3c3d04bc0ba9629f70ab50e49986d8a
0xXPunkX <35703636+0xXPunkX@users.noreply.github.com>
2023-02-10 10:47:26 +0800

    del bd ai

--- bot/bot_factory.py
+++ bot/bot_factory.py
@@ -9,12 +9,7 @@ def create_bot(bot_type):
     :param channel_type: channel type code
     :return: channel instance
     """
-    if bot_type == 'baidu':
-        # Baidu Unit对话接口
-        from bot.baidu.baidu_unit_bot import BaiduUnitBot
-        return BaiduUnitBot()
-
-    elif bot_type == 'chatGPT':
+    if bot_type == 'chatGPT':
         # ChatGPT 网页端web接口
         from bot.chatgpt.chat_gpt_bot import ChatGPTBot
         return ChatGPTBot()
